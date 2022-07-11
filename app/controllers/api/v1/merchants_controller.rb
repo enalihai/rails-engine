5ad389c7fae5e997f0e7054f2ce9af9a0c4f4b70
Luke Pascale <lpluke5@gmail.com>
2022-07-11 16:50:41 -0500

    requests/merchants: Setup merchant request path and models as well as Schema and controller

---
+++ app/controllers/api/v1/merchants_controller.rb
@@ -0,0 +1,42 @@
+class Api::V1::MerchantsController < ApplicationController
+  before_action :set_api_v1_merchant, only: [:show, :update, :destroy]
+
+  def index
+    render json: Merchant.all
+  end
+  #
+  # def show
+  #   render json: @api_v1_merchant
+  # end
+  #
+  # def create
+  #   @api_v1_merchant = Api::V1::Merchant.new(api_v1_merchant_params)
+  #
+  #   if @api_v1_merchant.save
+  #     render json: @api_v1_merchant, status: :created, location: @api_v1_merchant
+  #   else
+  #     render json: @api_v1_merchant.errors, status: :unprocessable_entity
+  #   end
+  # end
+  #
+  # def update
+  #   if @api_v1_merchant.update(api_v1_merchant_params)
+  #     render json: @api_v1_merchant
+  #   else
+  #     render json: @api_v1_merchant.errors, status: :unprocessable_entity
+  #   end
+  # end
+  #
+  # def destroy
+  #   @api_v1_merchant.destroy
+  # end
+  #
+  # private
+  #   def set_api_v1_merchant
+  #     @api_v1_merchant = Api::V1::Merchant.find(params[:id])
+  #   end
+  #
+  #   def api_v1_merchant_params
+  #     params.fetch(:api_v1_merchant, {id: params[:id], name: params[:name]})
+  #   end
+end
