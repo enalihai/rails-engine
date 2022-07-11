bdc67afbf469ecfd2c730017a9b789c507073dc0
Luke Pascale <lpluke5@gmail.com>
2022-07-11 18:17:46 -0500

    requests/merchant_item_request: Return a specific merchants items / add serializer for items

--- spec/requests/api/v1/merchants_spec.rb
+++ spec/requests/api/v1/merchants_spec.rb
@@ -38,5 +38,28 @@ RSpec.describe "Merchants API", type: :request do
       expect(merchant[:data][:attributes]).to be_a(Hash)
       expect(merchant[:data][:attributes][:name]).to be_a(String)
     end
+
+    it 'can return a merchants items' do
+      merchant1 = Merchant.create!(name: "Luke's Shop")
+      merchant2 = Merchant.create!(name: "Stephen's Shop")
+      item1 = merchant1.items.create!(description: "Breakfast food", name: "Waffles", unit_price: 21)
+      item2 = merchant1.items.create!(description: "Lunch Food", name: "PBJ", unit_price: 13)
+      item3 = merchant2.items.create!(description: "Dinner Food", name: "Steak", unit_price: 27)
+      item4 = merchant2.items.create!(description: "Dessert food", name: "Ice Cream", unit_price: 7)
+
+      get "/api/v1/merchants/#{merchant1.id}/items"
+
+      expect(response).to be_successful
+      item_list = JSON.parse(response.body, symbolize_names: true)
+
+      expect(item_list[:data].count).to eq(2)
+
+      item_list[:data].each do |item|
+        expect(item[:id]).to be_a(String)
+        expect(item[:attributes][:name]).to be_a(String)
+        expect(item[:attributes][:description]).to be_a(String)
+        expect(item[:attributes][:unit_price]).to be_a(Float)
+      end
+    end
   end
 end
