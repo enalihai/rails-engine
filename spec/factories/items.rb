fa912a5fa74b3a5560d796051e33c5c81824a870
Luke Pascale <lpluke5@gmail.com>
2022-07-11 19:14:50 -0500

    requests/items: Return all items based on Postman spec harness

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -3,6 +3,6 @@ FactoryBot.define do
     name {Faker::Name.name}
     description {Faker::Books::Dune.quote}
     unit_price {Faker::Number.number(digits: 4)}
-    merchant_id {Faker::Number.between(from: 1, to: 10)}
+    merchant
   end
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -7,7 +7,6 @@ RSpec.describe Item, type: :model do
 
   describe 'validates' do
     it { should validate_presence_of(:name)}
-    it { should validate_uniqueness_of(:name)}
     it { should validate_presence_of(:description)}
     it { should validate_presence_of(:unit_price)}
     it { should validate_presence_of(:merchant_id)}
