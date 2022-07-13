8a60912c2cbdf1e1d4cbe91145fca7a5b49cdd9d
Luke Pascale <lpluke5@gmail.com>
2022-07-13 05:59:29 -0500

    spec/models: Setup RSpec model testing for all 6 tables in prep for AR/SQL model methods

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -3,10 +3,11 @@ require 'rails_helper'
 RSpec.describe Merchant, type: :model do
   describe 'relationships' do
     it { should have_many(:items)}
+    it { should have_many(:invoices)}
+    it { should have_many(:customers).through(:invoices)}
   end
 
   describe 'validations' do
     it { should validate_presence_of(:name)}
-    it { should validate_uniqueness_of(:name)}
   end
 end
