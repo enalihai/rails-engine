99b442eeef85dcc09601b4d40e576055eb7acfc3
Luke Pascale <lpluke5@gmail.com>
2022-07-13 01:23:59 -0500

    controllers/items/find: Get a 204 status at least now, try to refactor to make Postman pass

--- app/controllers/api/v1/items/find_controller.rb
+++ app/controllers/api/v1/items/find_controller.rb
@@ -1,5 +1,6 @@
 class Api::V1::Items::FindController < ApplicationController
   def index
     item = Item.where(name: params[:name]).first
+    render json: ItemSerializer.new(item)
   end
 end
--- spec/requests/api/v1/search_request_spec.rb
+++ spec/requests/api/v1/search_request_spec.rb
@@ -2,111 +2,69 @@ require 'rails_helper'
 
 RSpec.describe 'Merchant and Item Search' do
   describe 'GET /items/find request' do
-  #   # before :each do
-  #   #   merchant = Merchant.create!(name: 'Test Merchant')
-  #   #   item_1 = merchant.items.create!({
-  #   #     name: 'Candlestick',
-  #   #     description: 'Holds your candles',
-  #   #     unit_price: 40.05
-  #   #   })
-  #   #   item_2 = merchant.items.create!({
-  #   #     name: 'desk',
-  #   #     description: 'Holds your documents',
-  #   #     unit_price: 38.85
-  #   #   })
-  #   #   item_3 = merchant.items.create!({
-  #   #     name: 'Rope bag',
-  #   #     description: 'Holds your rope',
-  #   #     unit_price: 42.75
-  #   #   })
-  #   #   item_4 = merchant.items.create!({
-  #   #     name: 'Gym bag',
-  #   #     description: 'Holds your gym clothes',
-  #   #     unit_price: 32.74
-  #   #   })
-  #   #   item_5 = merchant.items.create!({
-  #   #     name: 'Suitcase',
-  #   #     description: 'Holds your clothes',
-  #   #     unit_price: 38.04
-  #   #   })
-  #   # end
-  #
-  #   it 'returns single item that matches search term' do
-  #     merchant = Merchant.create!(name: 'Test Merchant')
-  #     item_1 = merchant.items.create!({
-  #       name: 'Candlestick',
-  #       description: 'Holds your candles',
-  #       unit_price: 40.05
-  #     })
-  #     item_2 = merchant.items.create!({
-  #       name: 'desk',
-  #       description: 'Holds your documents',
-  #       unit_price: 38.85
-  #     })
-  #     item_3 = merchant.items.create!({
-  #       name: 'Rope bag',
-  #       description: 'Holds your rope',
-  #       unit_price: 42.75
-  #     })
-  #     item_4 = merchant.items.create!({
-  #       name: 'Gym bag',
-  #       description: 'Holds your gym clothes',
-  #       unit_price: 32.74
-  #     })
-  #     item_5 = merchant.items.create!({
-  #       name: 'Suitcase',
-  #       description: 'Holds your clothes',
-  #       unit_price: 38.04
-  #     })
-  #
-  #     search_params = {name: 'desk'}
-  #     headers = {'CONTENT_TYPE' => 'application/json'}
-  #
-  #     get '/api/v1/items/find', headers: headers, params: search_params
-  #     response_body = JSON.parse(response.body, symbolize_names: true)
-  #     item = response.body
-  #
-  #     expect(response).to be_successful
-  #     expect(item).to have_key([:data])
-  #     expect(item[:data]).to be_a(Hash)
-  #     expect(item[:data]).to have_key([:attributes])
-  #
-  #     item_attributes = item[:data][:attributes]
-  #
-  #     expect(item_attributes).to have_key([:name])
-  #     expect(item_attributes[:name]).to eq('desk')
-  #     expect(item_attributes[:name]).to not_eq('Candlestick')
-  #     expect(item_attributes[:name]).to not_eq('Rope bag')
-  #     expect(item_attributes[:name]).to not_eq('Gym bag')
-  #     expect(item_attributes[:name]).to not_eq('Suitcase')
-  #
-  #     expect(item_attributes).to have_key([:description])
-  #     expect(item_attributes[:description]).to eq('desk')
-  #     expect(item_attributes[:description]).to not_eq('Candlestick')
-  #     expect(item_attributes[:description]).to not_eq('Rope bag')
-  #     expect(item_attributes[:description]).to not_eq('Gym bag')
-  #     expect(item_attributes[:description]).to not_eq('Suitcase')
-  #
-  #     expect(item_attributes).to have_key([:unit_price])
-  #     expect(item_attributes[:unit_price]).to eq('desk')
-  #     expect(item_attributes[:unit_price]).to not_eq('Candlestick')
-  #     expect(item_attributes[:unit_price]).to not_eq('Rope bag')
-  #     expect(item_attributes[:unit_price]).to not_eq('Gym bag')
-  #     expect(item_attributes[:unit_price]).to not_eq('Suitcase')
-  #
-  #     expect(item_attributes).to have_key([:merchant_id])
-  #     expect(item_attributes[:merchant_id]).to eq(@merchant.id)
-  #   end
+    it 'returns single item that matches search term' do
+      merchant = Merchant.create!(name: 'Test Merchant')
+      item_1 = merchant.items.create!({
+        name: 'Candlestick',
+        description: 'Holds your candles',
+        unit_price: 40.05
+      })
+      item_2 = merchant.items.create!({
+        name: 'desk',
+        description: 'Holds your documents',
+        unit_price: 38.85
+      })
+      item_3 = merchant.items.create!({
+        name: 'Rope bag',
+        description: 'Holds your rope',
+        unit_price: 42.75
+      })
+      item_4 = merchant.items.create!({
+        name: 'Gym bag',
+        description: 'Holds your gym clothes',
+        unit_price: 32.74
+      })
+      item_5 = merchant.items.create!({
+        name: 'Suitcase',
+        description: 'Holds your clothes',
+        unit_price: 38.04
+      })
 
-    it 'allows to specify a name query parameter'
-    it 'allows name: query parameter to be case insensitive'
-    it '?min_price# should return equal to or greater than given price'
-    it '?max_price# should return equal to or less than given price'
-    it '?max_price=X&min_price=Y can use min and max price in query'
-    it '?name=Name returns returns alphabetically'
-    it 'allows to search by one or more price-related query parameters'
-    it 'uses EITHER name param OR either/both price params'
-    it 'uses BOTH name param AND either/both price params returns error'
+      search_params = {name: 'desk'}
+      headers = {'CONTENT_TYPE' => 'application/json'}
+
+      get '/api/v1/items/find', headers: headers, params: search_params
+
+      expect(response).to be_successful
+
+      item = JSON.parse(response.body, symbolize_names: true)
+
+      expect(item[:data]).to be_a(Hash)
+      expect(item[:data][:attributes].count).to eq(4)
+      expect(item[:data][:type]).to eq('item')
+
+      item_attributes = item[:data][:attributes]
+
+      expect(item_attributes[:name]).to eq('desk')
+      expect(item_attributes[:name]).to_not eq('Candlestick')
+      expect(item_attributes[:name]).to_not eq('Rope bag')
+      expect(item_attributes[:name]).to_not eq('Gym bag')
+      expect(item_attributes[:name]).to_not eq('Suitcase')
+
+      expect(item_attributes[:description]).to eq('Holds your documents')
+      expect(item_attributes[:description]).to_not eq('Holds your candles')
+      expect(item_attributes[:description]).to_not eq('Holds your rope')
+      expect(item_attributes[:description]).to_not eq('Holds your gym clothes')
+      expect(item_attributes[:description]).to_not eq('Holds your clothes')
+
+      expect(item_attributes[:unit_price]).to eq(38.85)
+      expect(item_attributes[:unit_price]).to_not eq(40.05)
+      expect(item_attributes[:unit_price]).to_not eq(42.75)
+      expect(item_attributes[:unit_price]).to_not eq(32.74)
+      expect(item_attributes[:unit_price]).to_not eq(38.04)
+
+      expect(item_attributes[:merchant_id]).to eq(merchant.id)
+    end
   end
 
   describe 'GET /merchants/find request' do
