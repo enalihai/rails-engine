4233ba3a1de24649bcf2b42d05fdb66fd4baccc0
Luke Pascale <lpluke5@gmail.com>
2022-07-13 00:59:44 -0500

    config/routes: Setup routes for (hopefully) the last time, able to finish out rspec after

--- config/routes.rb
+++ config/routes.rb
@@ -1,22 +1,22 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
+      namespace :merchants do
+        resources :find, only: [:index]
+        resources :find_all, only: [:index]
+      end
+
+      namespace :items do
+        resources :find, only: [:index]
+        resources :find_all, only: [:index]
+      end
+
       resources :merchants, only: [:index, :show] do
         resources :items, only: [:index], controller: :merchant_items
       end
 
-      namespace :merchants do
-        resources :find, only: [:show]
-        resources :find_all, only: [:index]
-      end
-
-      resources :items do
+      resources :items, only: [:index, :show, :create, :update, :destroy] do
         resources :merchant, only: [:index], controller: :item_merchants
-      end
-
-      namespace :items do
-        resources :find, only: [:show]
-        resources :find_all, only: [:index]
       end
     end
   end
