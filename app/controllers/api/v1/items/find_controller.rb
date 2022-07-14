96599a873ba887ba6535fd7a675f5aa5b68d6b8d
Luke Pascale <lpluke5@gmail.com>
2022-07-13 20:58:38 -0500

    cleanup: Remove unnecessary controllers merchants::find_all#index and items::find_all#index

--- app/controllers/api/v1/items/find_controller.rb
+++ app/controllers/api/v1/items/find_controller.rb
@@ -1,9 +1,17 @@
 class Api::V1::Items::FindController < ApplicationController
   def index
-    #active record call here
+    items = Item.where('name ILIKE ?', "%#{params[:name]}%")
+    if item == nil
+      render json: ErrorSerializer.no_results_found
+    else
+      render json: ItemSerializer.new(items)
+    end
+  end
+
+  def show
     item = Item.where('name ILIKE ?', "%#{params[:name]}%").first
     if item == nil
-      render json: ErrorSerializer.errors
+      render json: ErrorSerializer.no_results_found
     else
       render json: ItemSerializer.new(item)
     end
--- app/serializers/error_serializer.rb
+++ app/serializers/error_serializer.rb
@@ -1,10 +1,20 @@
 class ErrorSerializer
-  def self.errors
+  def self.invalid_parameters
     {
     data:
       {
         id: 'error',
         title: 'Invalid input'
+      }
+    }
+  end
+
+  def self.no_results_found
+    {
+    data:
+      {
+        id: 'error',
+        title: "No results found for user input"
       }
     }
   end
